d502803f30c845f4707b1b6a3566b99b800cea8c
Yash Tiwari <yashutiwari22@gmail.com>
2024-09-26 12:13:48 +0530

    Update 5.2 Handling errors.js

--- 5.2 Handling errors.js
+++ 5.2 Handling errors.js
@@ -1,4 +1,16 @@
 //There is another parameter in this fnc: 
 app.use("/",(err,re,res,next)=>{
-  
-})
+  if(err) {
+    //also we can log the errors
+    res.status(500).send("Something went Wrong"));}
+  //but the good way is to use try catch
+});
+
+app.get(.....{
+  try{
+//Logic of db calls
+  }
+  catch(err){
+     res.status(500).send("Something went Wrong"));
+  }
+});
